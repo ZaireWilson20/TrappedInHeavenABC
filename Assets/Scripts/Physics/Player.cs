b32614dc019b2148a05a469feff0e6f6fa20bbc3
Zaire <Zaire.R.Wilson@gmail.com>
2019-05-14 00:24:13 -0400

    Init

---
+++ Assets/Scripts/Physics/Player.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Prime31
+{
+    [RequireComponent(typeof(CharacterController2D))]
+    public class Player : MonoBehaviour
+    {
+
+        public float jumpHeight = 4;
+        public float timeToJumpApex = .4f;
+        float accelerationTimeAirborne = .2f;
+        float accelerationTimeGrounded = .1f;
+        public float moveSpeed = 6;
+
+        float gravity;
+        [HideInInspector]
+        public float jumpVelocity;
+        public Vector3 velocity;
+        float velocityXSmoothing;
+
+        public CharacterController2D controller;
+
+        void Start()
+        {
+            controller = GetComponent<CharacterController2D>();
+
+            gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+            jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+            print("Gravity: " + gravity + "  Jump Velocity: " + jumpVelocity);
+        }
+
+        void Update()
+        {
+            //Debug.Log("Horizontal Collision: " + controller.collisions.right);
+            if (controller.isGrounded)
+            {
+                velocity.y = 0;
+            }
+
+
+            
+            if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+            {
+                velocity.y = jumpVelocity;
+            }
+            /*
+            if (!controller.collisions.below)
+            {
+                //velocity.x = 0; 
+            }
+            */
+            Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+            float targetVelocityX = input.x * moveSpeed;
+            velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, accelerationTimeGrounded);
+            velocity.y += gravity * Time.deltaTime;
+            controller.move(velocity * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Assets/Scripts/ProjectileGun.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prime31 {
+    public class ProjectileGun : MonoBehaviour
+    {
+        public float projectionSpeed;
+        private Vector3 gunPosVelocity;
+        Player player;
+        bool init = false;
+        bool projected = false;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+            player = GetComponent<GunController>().player.GetComponent<Player>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            Debug.Log("Angle between ground and gun: " + Vector2.Angle(player.gameObject.transform.position.normalized, transform.position.normalized));
+            gunPosVelocity = new Vector3(player.transform.position.x - transform.position.x + ((player.transform.position.x - transform.position.x < 0) ? -projectionSpeed : projectionSpeed), player.transform.position.y - transform.position.y + projectionSpeed);
+            //gunPosVelocity = (player.transform.position - gunPosVelocity);
+            //gunPosVelocity = new Vector3(projectionSpeed * gunPosVelocity.x, projectionSpeed * gunPosVelocity.y);
+
+
+
+            if (Input.GetMouseButtonDown(1) && !projected)
+            {
+                Debug.Log("gun pos " + transform.position + " player veloc " + player.jumpVelocity + " difference veloc " + gunPosVelocity);
+                projected = true;
+                player.controller.Project(ref player.velocity, projectionSpeed * (gunPosVelocity), true, ref projected);
+            }
+            else if (projected)
+            {
+                Debug.Log("hi");
+                player.controller.Project(ref player.velocity, gunPosVelocity, false, ref projected);
+                if (player.controller.isGrounded)
+                {
+                    projected = false;
+                }
+            }
+            
+            //Debug.Log("Gun is " + (player.transform.position - gunDirection));
+    }
+
+        void ProjectPlayer()
+        {
+
+        }
+    }
+}
